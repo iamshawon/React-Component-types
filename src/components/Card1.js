4e0e0bfb3d088e33062cde99fadfae758228c1be
iamshawon <shawon.main@gmail.com>
2021-08-21 22:18:00 +0600

    first commit

---
+++ src/components/Card1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div>
+            <h3>{props.name}</h3> <hr></hr>
+        </div>
+    )
+}
---
+++ src/components/Card2.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Card2 extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is Card 2</h3>
+            </div>
+        )
+    }
+}
